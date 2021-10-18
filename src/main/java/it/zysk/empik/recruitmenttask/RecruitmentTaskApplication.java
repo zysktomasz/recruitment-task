32aa07ed311039f575c602c29950caf724bb242b
Tomasz Zysk <zysk.tomasz95@gmail.com>
2021-10-18 19:35:56 +0200

    Create initial Spring project

---
+++ src/main/java/it/zysk/empik/recruitmenttask/RecruitmentTaskApplication.java
@@ -0,0 +1,13 @@
+package it.zysk.empik.recruitmenttask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/zysk/empik/recruitmenttask/RecruitmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package it.zysk.empik.recruitmenttask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
